01233fd4856ddced4900f869199e16904890cdef
Guillem <guillem@disedit.com>
2021-10-19 13:57:13 +0200

    Update meta tags

--- content/meta.js
+++ content/meta.js
@@ -1,6 +1,6 @@
 const title = 'Per un port sostenible - Compromís'
-const description = 'Sabries indicar en el mapa on es troben aquests punts del nostre territori? Una pista: tots es troben en risc per culpa de l\'especulació i les obres faraòniques depredadores i innecessàries.'
+const description = 'No a l\'ampliació del port. Perquè volem un port per a València i no contra València.'
 const keywords = 'port, valencia, ampliacio, ampliacion, puerto, sostenible, compromis, ecologisme, ecologismo'
-const url = 'https://portsostenible.compromis.net/'
+const url = 'https://portsostenible.com/'
 
 export { title, description, keywords, url }

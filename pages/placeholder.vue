f15958ba49878f25c3b1ee3c8d7760b76ec233ae
Carles <carles2612@gmail.com>
2021-10-20 10:11:03 +0200

    Release demo

---
+++ pages/placeholder.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="placeholder">
+    <h1>NO A L'AMPLIACIÓ DEL PORT</h1>
+    <p>Estem en construcció</p>
+    <p>Torna en uns dies ;)</p>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+.placeholder {
+  background: $green;
+  min-height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+
+  h1 {
+    font-family: $headings-font-family;
+    font-size: clamp(3rem, 12vw, 15rem);
+  }
+}
+</style>
